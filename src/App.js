6aa7668fbe01e07da7cab7f294bfd888a09f23ef
yujianxia <yujian.xia@foxmail.com>
2020-05-21 17:30:47 +0800

    添加令牌

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.yujianxia666
+          Edit <code>src/App.js</code> and save to reload.yujianxia888
         </p>
         <a
           className="App-link"
